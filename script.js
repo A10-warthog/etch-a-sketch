4ce61a321c92c573c7cfe4c2504907af97022fd3
A10-warthog <behemoth11111111@gmail.com>
2022-12-09 14:53:32 +0530

    Add parameter setValue with default value of 16

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function gridMain() {
     const grid = document.querySelector(".grid__body");
     const button_array = document.querySelectorAll("button");
 
-    function createGrid() {
+    function createGrid(setValue = 16) {
         const gridArea_int = grid.offsetWidth / 16;
     }
 }
