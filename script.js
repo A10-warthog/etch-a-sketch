c79dc977353a2d1362a15532df276b3832e2208d
A10-warthog <behemoth11111111@gmail.com>
2022-12-09 18:00:28 +0530

    Create function elmListener

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ function gridMain() {
     const grid = document.querySelector(".grid__body");
     const button_array = document.querySelectorAll("button");
     const defaultName = grid.previousElementSibling.textContent;
+    
+    function elmListener(elm, func) {
+        
+    }
+    
     function assignCssProp(elm, cssProp) {
         Object.assign(elm.style, cssProp);
     }
