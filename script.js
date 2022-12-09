bd2c1298a6ab99f0e030532a8fd71ebf074f67df
A10-warthog <behemoth11111111@gmail.com>
2022-12-09 14:56:20 +0530

    Add variable to create row in outer loop

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function gridMain() {
     function createGrid(setValue = 16) {
         const gridArea_int = grid.offsetWidth / 16;
         for(let i = 0; i < setValue; i++) {
+            const row = document.createElement("div");
             for (let j = 0; j < setValue; j++) {
                 
             }
