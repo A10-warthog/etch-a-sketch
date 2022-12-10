a6479c20b9b903a937d996ac8d3419ea37e05b2d
A10-warthog <behemoth11111111@gmail.com>
2022-12-10 21:39:00 +0530

    Add equals to sign in input checking statement

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ function gridMain() {
         } 
         const inputValue = Number(document.querySelector(".grid_input").value);
 
-        if(inputValue > 10 && inputValue < 100)
+        if(inputValue >= 10 && inputValue <= 100)
             removeGrid();
         else 
             console.log("Enter input value between 10 & 100");
