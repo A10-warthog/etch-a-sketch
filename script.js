89a24a38e8a2907e5f566fac0d177818af94715d
A10-warthog <behemoth11111111@gmail.com>
2022-12-10 17:52:42 +0530

    Change declaration of colorName

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function gridMain() {
     const grid = document.querySelector(".grid__body");
     const button_array = document.querySelectorAll("button");
-    const colorName = grid.previousElementSibling.textContent;
+    let colorName = grid.previousElementSibling.textContent;
 
     function assignCssProp(elm, cssProp) {
         Object.assign(elm.style, cssProp);
