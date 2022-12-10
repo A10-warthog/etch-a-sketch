8180a9735bf1b1452578a691a86b8227f5e3c088
A10-warthog <behemoth11111111@gmail.com>
2022-12-10 14:32:20 +0530

    Add return statement in randomColor
    The return value is in hsl format

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ function gridMain() {
 
     function randomColor() {
         const hue = Math.floor(Math.random() * 255);
+        return `hsl(${hue}, 100%, 50%)`;
     }
 
     function colorDiv(evt) {
