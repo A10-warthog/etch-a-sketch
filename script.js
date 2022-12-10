dcc8a6629b42bd316680897a80fb4c34459f0d7c
A10-warthog <behemoth11111111@gmail.com>
2022-12-10 22:07:05 +0530

    Add case for Eraser

--- script.js
+++ script.js
@@ -57,6 +57,9 @@ function gridMain() {
                 case "Default":
                     propName = "#000";
                     break;
+                case "Eraser":
+                    propName = "#000";
+                    break;
             }
         }
             
